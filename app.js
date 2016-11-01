4b8d510e13e5916481e9b2b827a80736f6d3def9
ad66216 <ad66216@gmail.com>
2016-11-01 17:36:24 -0400

    Updated JS file

--- app.js
+++ app.js
@@ -23,18 +23,18 @@ $(document).ready(function() {
 
 
 
-var fizz = "fizz";
-	var buzz = "buzz";
-	for (var i = 0; i < 100; i++) {
-		if(i % 3 == 0) {
-			if(i % 5 == 0) {
-				console.log("fizzBuzz");
-			}
-		} else if (i % 3 == 0) {
-			console.log("fizz");
-		} else if (i % 5 == 0) {
-			console.log("buzz");
-		} else {
-			console.log(i);
-		}
-	}
+// var fizz = "fizz";
+// 	var buzz = "buzz";
+// 	for (var i = 0; i < 100; i++) {
+// 		if(i % 3 == 0) {
+// 			if(i % 5 == 0) {
+// 				console.log("fizzBuzz");
+// 			}
+// 		} else if (i % 3 == 0) {
+// 			console.log("fizz");
+// 		} else if (i % 5 == 0) {
+// 			console.log("buzz");
+// 		} else {
+// 			console.log(i);
+// 		}
+// 	}
